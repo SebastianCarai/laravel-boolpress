5600701e9a3e29ba0e6328178c9b8f84d45e4549
SebastianCarai <sebastiancarai7@gmail.com>
2022-02-24 13:08:47 +0100

    bonus 1

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>Bevenuto {{ Auth::user()->name }}</h1>
+    <h1>Bevenuto {{ $user->name }}</h1>
+
+    @if ($userInfo)
+        <h4>Phone number: {{$userInfo->phone_number}}</h4>
+        <h4>Address: {{$userInfo->address}}</h4>
+    @endif
 @endsection
