04325724cd252d0657e218c438362e1207845e74
SebastianCarai <sebastiancarai7@gmail.com>
2022-03-03 16:39:45 +0100

    custom header navbar

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <span class="navbar-brand" href="#">Navbar</span>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,6 +15,13 @@
                         </ul>
                     </div>
                 </div>
+            </nav> -->
+            <nav>
+                <ul class="header_menu">
+                    <li v-for="(link,index) in routes" :key="index">
+                        <router-link class="active link" :to="{name: link.name}">{{link.label}}</router-link>
+                    </li>
+                </ul>
             </nav>
         </div>
     </header>
@@ -43,3 +50,26 @@ export default {
     }
 }
 </script>
+
+<style scoped lang="scss">
+.header_menu{
+    list-style-type: none;
+    margin: 10px 0 30px;
+    padding: 0;
+    li{
+        display: inline-block;
+        padding-right: 30px;
+        padding-top: 10px;
+        font-size: 18px;
+        line-height: 50px;
+        .link{
+            height: inherit;
+            display: inline-block;
+            &:hover{
+                text-decoration: none;
+            }
+        }
+
+    }
+}
+</style>
